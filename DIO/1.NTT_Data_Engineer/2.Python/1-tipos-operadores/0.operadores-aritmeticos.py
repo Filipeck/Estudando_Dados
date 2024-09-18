268a6117f4ac7dc118225e8e6eec39b40aace284
Filipe Cayres <contato.fcayres@gmail.com>
2024-09-17 22:45:03 -0300

    dando sequencia em python, começando operadores

---
+++ DIO/1.NTT_Data_Engineer/2.Python/1-tipos-operadores/0.operadores-aritmeticos.py
@@ -0,0 +1,16 @@
+produto_1 = 10
+produto_2 = 20
+
+soma = produto_1 + produto_2
+subtracao = produto_1 - produto_2
+divisao = produto_2 / produto_1
+divisao_inteira = produto_2 // produto_1
+multiplicacao = produto_1 * produto_2
+divisao_resto = produto_2 % produto_1
+exponenciacao = produto_2 ** produto_1
+
+print(soma,subtracao,divisao,divisao_inteira, multiplicacao, divisao_resto, exponenciacao,sep='\n')
+
+x = (10 + 5) * 4
+y = (10 / 2) + (25 * 2) - (2 ** 2)
+print(x,y, sep=' | ')
