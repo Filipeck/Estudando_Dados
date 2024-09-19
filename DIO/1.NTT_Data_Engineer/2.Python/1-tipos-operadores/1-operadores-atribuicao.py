4c98c876530376fedd5b34ba359d11b910c2553e
Filipe Cayres <contato.fcayres@gmail.com>
2024-09-18 21:30:50 -0300

    continuando operadores python

---
+++ DIO/1.NTT_Data_Engineer/2.Python/1-tipos-operadores/1-operadores-atribuicao.py
@@ -0,0 +1,34 @@
+saldo = 500
+
+print(saldo)
+
+saldo += 200
+
+print(saldo)
+
+saldo *= 0.10
+
+print(saldo)
+
+saldo /= 2
+print(saldo)
+
+saldo += 500
+
+print(saldo)
+
+saldo -= 150
+
+print(saldo)
+
+saldo //= 3
+
+print(saldo)
+
+saldo %= 3
+
+print(saldo)
+
+saldo **= 2
+
+print(saldo)
