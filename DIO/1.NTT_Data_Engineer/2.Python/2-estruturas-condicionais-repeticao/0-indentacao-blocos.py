4fda487703774b8a6ec1ea4a4bb9fb33a4b4d6e5
Filipe Cayres <contato.fcayres@gmail.com>
2024-09-19 21:39:24 -0300

    iniciando módulo

---
+++ DIO/1.NTT_Data_Engineer/2.Python/2-estruturas-condicionais-repeticao/0-indentacao-blocos.py
@@ -0,0 +1,9 @@
+def sacar(valor):
+    saldo = 500
+
+    if saldo >= valor:
+        print('Valor sacado!')
+        print('Retire o seu dinheiro na boca do caixa.')
+    print('Obrigado por usar o nosso serviço, tenha um bom dia!')
+
+sacar(100)
