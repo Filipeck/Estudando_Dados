225525b6d6e5688001ff390a1510c11f38a4b5c1
Filipe Cayres <contato.fcayres@gmail.com>
2024-09-23 22:21:00 -0300

    multiplas linhas de strings

---
+++ DIO/1.NTT_Data_Engineer/2.Python/3-Strings/3-string-multiplas-linhas.py
@@ -0,0 +1,23 @@
+nome = "Filipe"
+
+mensagem = f'''
+Exemplo mostrando como
+funcionam strings de 
+     múltiplas linhas,
+         elas mantém os espaços.'''
+
+print(mensagem)
+
+print(
+    """
+    ======== MENU ======
+
+    1 - Depositar
+    2 - Sacar
+    0 - Sair
+
+    ===================
+
+       Obrigado por usar o nosso sistema!
+    """
+)
